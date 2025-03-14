4fe32bd9f8d4f3f5d3c5369e9b5d81e1c47dcab0
TruffleBug <lisa.ho1022@gmail.com>
2025-03-14 13:22:04 -0600

    Fin.

--- reference/http_demo.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-// CREATE SERVER OBJECT
-http.createServer((req, res) => {
-    // WRITE RESPONSE
-    res.write('Hello World');
-    res.end();
-}).listen(5000, () => console.log('Server running...'));
